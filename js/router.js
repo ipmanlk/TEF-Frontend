220a73fd187e9efa7f7ec7e78fa599c2fb3837b0
ipmanlk <io@navinda.xyz>
2020-03-19 20:08:48 +0530

    change: common.js routing function to seperate file

---
+++ js/router.js
@@ -0,0 +1,15 @@
+// load routes and change dashboard title appropriately
+const loadRoute = (path) => {
+    switch (path) {
+        case "dashboard":
+            path = "./pages/dashboard.html";
+            $("#txtNavbarTitle").text("Dashboard");
+            break;
+        case "employee":
+            path = "./pages/employee.html";
+            $("#txtNavbarTitle").text("Employee View");
+            break;
+    }
+
+    $("#iframeContent").attr("src", path);
+}
