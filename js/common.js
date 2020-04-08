fc0f77905cdebcd963b84078f4dedef88aec885f
ipmanlk <io@navinda.xyz>
2020-04-08 21:10:22 +0530

    change: refactor common.js

--- js/common.js
+++ js/common.js
@@ -1,102 +1,102 @@
-// validate element values using given regular expressions
-const validateElementValue = (elementValidationInfo) => {
+class FormUtil {
+    static validateElementValue(elementValidationInfo) {
+        // create selector name for ui element id
+        const selector = `#${elementValidationInfo.attribute}`;
 
-    // create selector name for ui element id
-    const selector = `#${elementValidationInfo.attribute}`;
+        // get value of element id
+        const value = $(selector).val();
 
-    // get value of element id
-    const value = $(selector).val();
+        // create RegExp object from regex string
+        const regex = new RegExp(elementValidationInfo.regex);
 
-    // create RegExp object from regex string
-    const regex = new RegExp(elementValidationInfo.regex);
+        // if value is optional and not set, ignore
+        if (elementValidationInfo.optional && value.trim() == "") {
+            $(selector).parent().removeClass("has-error has-success");
+            $(selector).parent().children("span").remove();
+            return true;
+        }
 
-    // if value is optional and not set, ignore
-    if (elementValidationInfo.optional && value.trim() == "") {
-        $(selector).parent().removeClass("has-error has-success");
-        $(selector).parent().children("span").remove();
-        return true;
+        // check form values with each regex
+        if (!regex.test(value)) {
+            $(selector).parent().removeClass("has-success");
+            $(selector).parent().addClass("has-error");
+            $(selector).parent().children("span").remove();
+            $(selector).parent().append(`<span class="glyphicon glyphicon-remove form-control-feedback"></span>`);
+            return false;
+        } else {
+            $(selector).parent().removeClass("has-error");
+            $(selector).parent().addClass("has-success");
+            $(selector).parent().children("span").remove();
+            $(selector).parent().append(`<span class="glyphicon glyphicon-ok form-control-feedback"></span>`);
+            return true;
+        }
     }
 
-    // check form values with each regex
-    if (!regex.test(value)) {
-        $(selector).parent().removeClass("has-success");
-        $(selector).parent().addClass("has-error");
-        $(selector).parent().children("span").remove();
-        $(selector).parent().append(`<span class="glyphicon glyphicon-remove form-control-feedback"></span>`);
-        return false;
-    } else {
-        $(selector).parent().removeClass("has-error");
-        $(selector).parent().addClass("has-success");
-        $(selector).parent().children("span").remove();
-        $(selector).parent().append(`<span class="glyphicon glyphicon-ok form-control-feedback"></span>`);
-        return true;
-    }
-}
-
-// general event listeners based on regexes and attributes received from server
-const registerGeneralEventListeners = (validationInfo) => {
-    // realtime validation
-    validationInfo.forEach(vi => {
-        $(`#${vi.attribute}`).on("keyup change", () => {
-            validateElementValue(vi);
+    static enableRealtimeValidation(validationInfo) {
+        // provide freedback when user interact with form elements
+        validationInfo.forEach(vi => {
+            $(`#${vi.attribute}`).on("keyup change", () => {
+                this.validateElementValue(vi);
+            });
         });
-    });
-
-    // prevent from submissions
-    $("form").on("submit", (e) => e.preventDefault());
+    }
 }
 
-// create image url from buffer data recived from server
-const getImageURLfromBuffer = (buffer) => {
-    const arrayBufferView = new Uint8Array(buffer.data);
-    const blob = new Blob([arrayBufferView], { type: "image/png" });
-    const urlCreator = window.URL || window.webkitURL;
-    const imageUrl = urlCreator.createObjectURL(blob);
-    return imageUrl;
+class ImageUtil {
+    static getURLfromBuffer(buffer) {
+        // create image url from buffer data recived from server
+        const arrayBufferView = new Uint8Array(buffer.data);
+        const blob = new Blob([arrayBufferView], { type: "image/png" });
+        const urlCreator = window.URL || window.webkitURL;
+        const imageUrl = urlCreator.createObjectURL(blob);
+        return imageUrl;
+    }
+
+    static getBase64FromFile(file) {
+        // create a base64 string from a file object
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = error => reject(error);
+        });
+    }
 }
 
-// create a base64 string from a file
-const getBase64FromFile = (file) => {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = error => reject(error);
-    });
-}
-
-// send http requests and handle errors
-const request = (path, method, data = {}) => {
-    return new Promise((resolve, reject) => {
-        const options = {
-            type: method,
-            contentType: "application/json; charset=utf-8",
-            url: `http://localhost:3000${path}`,
-            data: data,
-            dataType: "json"
-        }
-
-        // send json on post bodies
-        if (method == "POST" || method == "PUT" || method == "DELETE") {
-            options.data = JSON.stringify(data);
-        }
-
-        const req = $.ajax(options);
-
-        req.done((res) => {
-            if (res.status) {
-                resolve(res);
-            } else {
-                mainWindow.showOutputModal("Error", res.msg);
-                if (res.type == "auth") {
-                    window.location = "noauth.html"
-                }
+class Request {
+    static send(path, method, data = {}) {
+        // send http requests and handle errors
+        return new Promise((resolve, reject) => {
+            const options = {
+                type: method,
+                contentType: "application/json; charset=utf-8",
+                url: `http://localhost:3000${path}`,
+                data: data,
+                dataType: "json"
             }
-        });
 
-        req.fail((jqXHR, textStatus) => {
-            mainWindow.showOutputModal("Error", `Unable to retrive data from the server: ${textStatus}`);
-            reject(textStatus);
+            // send json on post bodies
+            if (method == "POST" || method == "PUT" || method == "DELETE") {
+                options.data = JSON.stringify(data);
+            }
+
+            const req = $.ajax(options);
+
+            req.done((res) => {
+                if (res.status) {
+                    resolve(res);
+                } else {
+                    mainWindow.showOutputModal("Error", res.msg);
+                    if (res.type == "auth") {
+                        window.location = "noauth.html"
+                    }
+                }
+            });
+
+            req.fail((jqXHR, textStatus) => {
+                mainWindow.showOutputModal("Error", `Unable to retrive data from the server: ${textStatus}`);
+                reject(textStatus);
+            });
         });
-    });
+    }
 }
