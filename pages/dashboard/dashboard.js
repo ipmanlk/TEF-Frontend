d429467a64f0f0df88be51d306345c210d3b4073
ipmanlk <io@navinda.xyz>
2020-08-14 12:18:41 +0530

    add: dashboard card filtering

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,3 +1,28 @@
+$(document).ready(function () {
+
+    // event listener for filtering cards
+    $("#txtFilterCards").on("keyup change", (e) => {
+        const keyword = e.target.value.trim().toLowerCase();
+
+        // when keyword is empty
+        if (keyword.trim() == "") {
+            updateTiles();
+            return;
+        }
+
+        $(".TILE").each((i, tile) => {
+            // check h1 text inside tile
+            const tileText = $(tile).first().first().first().text().toLowerCase();
+            if (tileText.indexOf(keyword) > -1) {
+                $(tile).show();
+            } else {
+                $(tile).hide();
+            }
+        });
+    });
+
+});
+
 function updateTiles() {
     // show hide tiles based on privileges
     const privileges = mainWindow.tempData.privileges;
@@ -6,7 +31,7 @@ function updateTiles() {
     $(".TILE").hide();
 
     // show tiles based on privileges
-    Object.keys(privileges).forEach(p => {        
+    Object.keys(privileges).forEach(p => {
         $(`.${p}.TILE`).show();
     });
 }
