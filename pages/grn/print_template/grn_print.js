8ea95acdefec1d53649da67bec7b2b92e5b4ba37
ipmanlk <io@navinda.xyz>
2020-10-16 12:49:59 +0530

    change: improved grn print

--- pages/grn/print_template/grn_print.js
+++ pages/grn/print_template/grn_print.js
@@ -18,22 +18,48 @@ const printGrn = async (selectedEntry) => {
 
   // replace template values
   const supplier = selectedEntry.purchaseOrder.quotation.quotationRequest.supplier;
+  console.log(supplier);
+  const placeholderValues = {
+    code: selectedEntry.grncode,
+    supType: supplier.supplierType.name,
+    addedDate: selectedEntry.addedDate,
+    receivedDate: selectedEntry.receivedDate,
+    supCompanyName: supplier.companyName,
+    supCompanyMobile: supplier.companyMobile,
+    supCompanyMail: supplier.email,
+    supCompanyAddress: supplier.address,
+    supCompanyPersonName: supplier.personName,
+    supCompanyPersonMobile: supplier.personMobile,
+    supName: supplier.personName,
+    supMobile: supplier.personMobile,
+    supMail: supplier.email,
+    supAddress: supplier.address,
+    materialRows: materialRows,
+    grandTotal: selectedEntry.grandTotal,
+    discountRatio: selectedEntry.discountRatio,
+    netTotal: selectedEntry.netTotal,
+    description: selectedEntry.description
+  }
 
-  template = template.replace(/#{grncode}/g, selectedEntry.grncode);
-  template = template.replace(/#{addedDate}/g, selectedEntry.addedDate);
-  template = template.replace(/#{receivedDate}/g, selectedEntry.receivedDate);
-  template = template.replace(/#{supplierCompanyName}/g, supplier.companyName);
-  template = template.replace(/#{supplierAddress}/g, supplier.address);
-  template = template.replace(/#{supplierCompanyMobile}/g, supplier.companyMobile);
-  template = template.replace(/#{supplierEmail}/g, supplier.email);
-  template = template.replace(/#{materialRows}/g, materialRows);
-  template = template.replace(/#{description}/g, selectedEntry.description.trim() == "" ? "None" : selectedEntry.description);
+  // fix description
+  if (!placeholderValues.description || placeholderValues.description.trim() == "") {
+    placeholderValues.description = "None";
+  }
 
-  template = template.replace(/#{grandTotal}/g, selectedEntry.grandTotal);
-  template = template.replace(/#{discountRatio}/g, selectedEntry.discountRatio);
-  template = template.replace(/#{netTotal}/g, selectedEntry.netTotal);
+  Object.keys(placeholderValues).forEach(key => {
+    template = template.replace(new RegExp(`#{${key}}`, "g"), placeholderValues[key]);
+  });
+
 
   const win = window.open("", "Print", "width=1000,height=600");
   win.document.write(template);
+
+  // get proper class name to hide
+  let className = (placeholderValues.supCompanyName) ? ".individual" : ".company";
+  let classes = win.document.querySelectorAll(className);
+  for (let i = 0; i < classes.length; i++) {
+    classes[i].style.display = "none";
+  }
+
   setTimeout(() => win.print(), 1000);
 }
