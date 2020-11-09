1ab62b9c5d0f1ede5516fa863497e568f0e0c9b0
ipmanlk <io@navinda.xyz>
2020-11-09 11:07:30 +0530

    add: dummy form to production inventory

--- pages/production_inventory/production_inventory.js
+++ pages/production_inventory/production_inventory.js
@@ -16,6 +16,7 @@ async function loadModule(permissionStr) {
 				"Qty. (All Time)": entry.qty,
 				"Avaiable Qty.": entry.availableQty,
 				Status: entry.productionInventoryStatus.name,
+				Edit: `<button class="btn btn-warning btn-sm" onclick="showEditEntryModal('${entry.id}')"><i class="glyphicon glyphicon-edit" aria-hidden="true"></i> Edit</button>`,
 			};
 		});
 	};
@@ -39,3 +40,69 @@ async function loadModule(permissionStr) {
 		);
 	});
 }
+
+/*-------------------------------------------------------------------------------------------------------
+                                            Modals
+-------------------------------------------------------------------------------------------------------*/
+
+const showNewEntryModal = () => {
+	// reset form values
+	resetForm();
+	FormUtil.disableReadOnly("mainForm");
+
+	FormUtil.setButtionsVisibility("mainForm", tempData.permission, "add");
+
+	// set created employee number
+	const employeeNumber = mainWindow.tempData.profile.employee.number;
+	const employeeCallingName = mainWindow.tempData.profile.employee.callingName;
+	$("#mainForm #createdEmployee").val(
+		`${employeeCallingName} (${employeeNumber})`
+	);
+	// set modal title
+	$("#modalMainFormTitle").text("Add New Production Order");
+	// set date of adding
+	$("#mainForm #addedDate").val(new Date().today());
+	// limit for required data
+	const date = new Date();
+	// const dueDate = $("#mainForm #requiredDate");
+	$("#mainForm #requiredDate").attr("min", date.formatForInput());
+	// dueDate.attr("max", date.addDays(30).formatForInput());
+
+	// empty code
+	$("#mainForm #code").val("Order code will be displayed after adding.");
+
+	$("#confirmedInfo").hide();
+
+	// show modal
+	$("#modalMainForm").modal("show");
+};
+
+const showEditEntryModal = (id, readOnly = false) => {
+	loadEntry(id).then(() => {
+		$("#modalMainForm").modal("show");
+
+		if (readOnly) {
+			FormUtil.enableReadOnly("mainForm");
+			FormUtil.setButtionsVisibility("mainForm", tempData.permission, "view");
+			$("#modalMainFormTitle").text("View Production Order");
+		} else {
+			FormUtil.disableReadOnly("mainForm");
+			FormUtil.setButtionsVisibility("mainForm", tempData.permission, "edit");
+			$("#modalMainFormTitle").text("Edit Production Order");
+			// hide form deleted button when deleted
+			if (tempData.selectedEntry.productionOrderStatus.name == "Deleted") {
+				$(".btnFmDelete").hide();
+			}
+		}
+
+		// confirmed info
+		if (
+			tempData.selectedEntry.confirmedBy != null &&
+			tempData.selectedEntry.confirmedDate != null
+		) {
+			$("#confirmedInfo").show();
+		} else {
+			$("#confirmedInfo").hide();
+		}
+	});
+};
