e21e07ae04ed3e4c9c864294f9c96292a1ed9cc5
ipmanlk <io@navinda.xyz>
2021-04-05 13:14:24 +0530

    fix: purchase order printout ui

--- pages/purchase_order/print_template/purchase_order_print.js
+++ pages/purchase_order/print_template/purchase_order_print.js
@@ -1,63 +1,71 @@
 const printPurchaseOrder = async (selectedEntry) => {
-  let materialRows = "";
-  selectedEntry.purchaseOrderMaterials.forEach((pom, index) => {
-    materialRows += `
+	let materialRows = "";
+	selectedEntry.purchaseOrderMaterials.forEach((pom, index) => {
+		materialRows += `
       <tr>
         <td>${index + 1}</td>
         <td>${pom.material.name}</td>
         <td>${pom.purchasePrice}</td>
         <td>${pom.qty}</td>
         <td>${pom.lineTotal}</td>
+        <td>${pom.material.unitType.name}</td>
       </tr>
-    `
-  });
+    `;
+	});
 
-  // load template
-  const respone = await fetch("./print_template/purchase_order_print.html");
-  let template = await respone.text();
+	// load template
+	const respone = await fetch("./print_template/purchase_order_print.html");
+	let template = await respone.text();
 
+	// replace template values
+	const supplier = selectedEntry.quotation.quotationRequest.supplier;
 
-  // replace template values
-  const supplier = selectedEntry.quotation.quotationRequest.supplier;
+	const placeholderValues = {
+		pocode: selectedEntry.pocode,
+		supCode: supplier.code,
+		supType: supplier.supplierType.name,
+		addedDate: selectedEntry.addedDate,
+		requiredDate: selectedEntry.requiredDate,
+		supCompanyName: supplier.companyName,
+		supCompanyMobile: supplier.companyMobile,
+		supCompanyMail: supplier.email,
+		supCompanyAddress: supplier.address,
+		supCompanyPersonName: supplier.personName,
+		supCompanyPersonMobile: supplier.personMobile,
+		supName: supplier.personName,
+		supMobile: supplier.personMobile,
+		supMail: supplier.email,
+		supAddress: supplier.address,
+		materialRows: materialRows,
+		description: selectedEntry.description,
+		total: selectedEntry.totalPrice,
+	};
 
-  const placeholderValues = {
-    pocode: selectedEntry.pocode,
-    supCode: supplier.code,
-    supType: supplier.supplierType.name,
-    addedDate: selectedEntry.addedDate,
-    requiredDate: selectedEntry.requiredDate,
-    supCompanyName: supplier.companyName,
-    supCompanyMobile: supplier.companyMobile,
-    supCompanyMail: supplier.email,
-    supCompanyAddress: supplier.address,
-    supCompanyPersonName: supplier.personName,
-    supCompanyPersonMobile: supplier.personMobile,
-    supName: supplier.personName,
-    supMobile: supplier.personMobile,
-    supMail: supplier.email,
-    supAddress: supplier.address,
-    materialRows: materialRows,
-    description: selectedEntry.description
-  }
+	// fix description
+	if (
+		!placeholderValues.description ||
+		placeholderValues.description.trim() == ""
+	) {
+		placeholderValues.description = "None";
+	}
 
-  // fix description
-  if (!placeholderValues.description || placeholderValues.description.trim() == "") {
-    placeholderValues.description = "None";
-  }
+	Object.keys(placeholderValues).forEach((key) => {
+		template = template.replace(
+			new RegExp(`#{${key}}`, "g"),
+			placeholderValues[key]
+		);
+	});
 
-  Object.keys(placeholderValues).forEach(key => {
-    template = template.replace(new RegExp(`#{${key}}`, "g"), placeholderValues[key]);
-  });
+	const win = window.open("", "Print", "width=1000,height=600");
+	win.document.write(template);
 
-  const win = window.open("", "Print", "width=1000,height=600");
-  win.document.write(template);
+	// get proper class name to hide
+	let className =
+		placeholderValues.supType == "Company" ? ".individual" : ".company";
+	let classes = win.document.querySelectorAll(className);
+	for (let i = 0; i < classes.length; i++) {
+		classes[i].style.display = "none";
+	}
 
-  // get proper class name to hide
-  let className = (placeholderValues.supType == "Company") ? ".individual" : ".company";
-  let classes = win.document.querySelectorAll(className);
-  for (let i = 0; i < classes.length; i++) {
-    classes[i].style.display = "none";
-  }
-
-  setTimeout(() => win.print(), 1000);
-}
+	setTimeout(() => win.print(), 1000);
+};
